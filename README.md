a807f395d16cfae5cf6521a8f31b96e1b6d2528f
Harihara Suthan <hariharasuthan.2505040@srec.ac.in>
2026-06-16 14:44:25 +0530

    packages completed

---
+++ README.md
@@ -0,0 +1,15 @@
+# Gosei Dataset
+
+## Features
+
+- Time Series Generation
+- Dataset Generation
+- Missing Value Simulation
+- Dataset Merging
+- Ensemble Learning
+
+## Installation
+
+pip install goseidataset
+
+## Quick Start
--- src/goseidataset/__init__.py
+++ src/goseidataset/__init__.py
@@ -1,1 +1,11 @@
 from .timeseries import TimeSeriesGenerator
+from .merge_dataset import Supervised_learning
+from .missingvaluegenerator import MissingValueGenerator
+from .generatedataset import DatasetGenerator
+
+__all__ = [
+    "TimeSeriesGenerator",
+    "Supervised_learning",
+    "MissingValueGenerator",
+    "DatasetGenerator"
+]
